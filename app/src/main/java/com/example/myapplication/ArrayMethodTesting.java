bd42c1756dddf036570d55aa9ec5531cbdf1f54a
Talha Rahman <talharahman@Talhas-MBP.fios-router.home>
2019-02-23 18:20:46 -0500

    new commit

--- app/src/main/java/com/example/myapplication/ArrayMethodTesting.java
+++ app/src/main/java/com/example/myapplication/ArrayMethodTesting.java
@@ -1,5 +1,8 @@
 package com.example.myapplication;
 
+import java.util.Map;
+import java.util.Set;
+
 public class ArrayMethodTesting {
 
     private static ArrayMethodTesting thisInstance;
@@ -13,5 +16,62 @@ public class ArrayMethodTesting {
         return thisInstance;
     }
 
+    /**
+     * return a boolean value
+     * if the argument passed to the method is less than 10 elements in length, return true, otherwise return false
+     */
+
+    public boolean isLessThan10(String[] input){
+
+    }
+
+    /**
+     * return a String[] value
+     * if the argument passed to the method has an even-numbered length, remove the String with the longest length
+     * return a new String[] with the expected values
+     */
+
+    public String[] smallEvensOnly(String[] input){
+
+    }
+
+    /**
+     * return a char[] value
+     * return a new char[] with the expected values
+     */
+
+    public char[] sortAlphabetically(char[] input){
+
+    }
+
+    /**
+     * return a double value
+     * return the sum of all the decimal numbers in the input array
+     */
+
+    public double returnSum(double[] input){
+
+    }
+
+    /**
+     * return a Set value
+     * return a set of only unique items in the input array
+     * if at least one version of the string from the array exists in the set
+     * (uppercase, lowercase, proper case, camel case, etc.), it is not unique, please don't add it to the set
+     */
+
+    public Set removeCaseSensitiveDuplicates(String[] input){
+
+    }
+
+    /**
+     * return a Map value
+     * return the Map containing every element of the array as the key,
+     * and the number of times it occurs in the array as its value
+     */
+
+    public Map mapDuplicates(String[] input){
+
+    }
 
 }
--- app/src/test/java/com/example/myapplication/ArrayMethodTestingTest.java
+++ app/src/test/java/com/example/myapplication/ArrayMethodTestingTest.java
@@ -19,7 +19,7 @@ public class ArrayMethodTestingTest {
 
     @After
     public void tearDown(){
-        arrayMethodTesting = null
+        arrayMethodTesting = null;
     }
 
 
